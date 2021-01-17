104ed7635ae484e0cc8b6f79e070743c3c31388a
brutal-snezhok <czyrulik97@mail.ru>
2021-01-17 12:10:40 +0300

    added db properties

--- src/main/java/com/example/demo/student/StudentModel.java
+++ src/main/java/com/example/demo/student/StudentModel.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
@@ -10,7 +11,15 @@ import java.time.LocalDate;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class StudentModel {
+
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
